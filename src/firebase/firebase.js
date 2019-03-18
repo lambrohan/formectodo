706ff6055d66c0406a124efc1612920b689d0c5b
Rohan Lamb <lambrohan@gmail.com>
2019-03-18 21:22:10 +0530

    finalizing things

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,11 +5,11 @@
 
  var config = {
   apiKey: "xxxxxxxxxxxxxxxxxx",
-  authDomain: "formec-work.firebaseapp.com",
-  databaseURL: "https://formec-work.firebaseio.com",
-  projectId: "formec-work",
-  storageBucket: "formec-work.appspot.com",
-  messagingSenderId: "17397114135"
+  authDomain: "xxxxxx.firebaseapp.com",
+  databaseURL: "xxxxxx",
+  projectId: "formec-xxx",
+  storageBucket: "xxxx.appspot.com",
+  messagingSenderId: "xxxxxx"
 };
 
 firebase.initializeApp(config);
